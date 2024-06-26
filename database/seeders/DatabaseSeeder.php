f768001c4ede3ef417d774ddd75cf68bd10305f1
Ali Nasser <ali.slman7.98@gmail.com>
2024-06-26 13:58:30 +0300

    insert some seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +19,27 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        DB::table('roles')->insert([
+            'id'=>1,
+            'name'=>'admin'
+        ]);
+        DB::table('roles')->insert([
+            'id'=>2,
+            'name'=>'data_entry'
+        ]);
+        DB::table('roles')->insert([
+            'id'=>3,
+            'name'=>'client'
+        ]);
+        DB::table('users')->insert([
+            'id'=>1,
+            'name'=>'admin',
+            'role_id'=>1,
+            'password'=>bcrypt("12345678"),
+            'email'=>'admin@admin.com',
+            'email_verified_at'=>now(),
+        ]);
+
+
     }
 }
