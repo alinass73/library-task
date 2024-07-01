cec373b9728ab4e6187ed3daa6638933981aebcc
Ali Nasser <ali.slman7.98@gmail.com>
2024-07-01 16:18:44 +0300

    service for auth done

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -3,37 +3,33 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Responses\Response;
 use App\Models\User;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    private UserService $userService;
+    
+    public function __construct(UserService $userService)
+    {
+        $this->userService= $userService;
+    }
+
     public function login(Request $request)
     {
+        $data= [];
         try{
-            $request->validate([
-                'email'=>['required','email'],
-                'password'=>['required']
-            ]);
-            if(!Auth::attempt($request->only(['email','password']))){
-                return $this->error('message','Credentials do not match',401);
-            }
-            $user= User::where('email',$request->email)->first();
-            
-            // $user['token']=$user->createToken("API TOKEN")->plainTextToken;
-            // return new UserResource($user);
-            return response()->json([
-                'user'=>$user,
-                'token'=>$user->createToken('Api Token of '. $user->name)->plainTextToken
-            ]);
-        }catch (\Throwable $th) {
-            return response()->json([
-                'status' => false,
-                'message' => $th->getMessage()
-        ], 500);
+            $data = $this->userService->login($request);
+            return Response::Success($data['user'], $data['message']);
+        }catch(\Throwable $th){
+            $message= $th->getMessage();
+            return Response::Error($data,$message);
+        }
     }
-    }
+ 
 
     public function s(){
          
@@ -42,42 +38,33 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
+        $data= [];
         try{
-            $request->validate([
+            
+            $data = $this->userService->register($request->validate([
                 'name'=>['required'],
                 'password'=>['required','confirmed'],
                 'email'=>['required','email'],
-            ]);
-            
-            $user=User::create([
-                'name'=>$request['name'],
-                'email'=>$request['email'],
-                'password'=>bcrypt($request['password']),
-                'role_id'=>3
-            ]);
-            $user->sendEmailVerificationNotification();
-            // return new UserResource($user);
-            return response()->json([
-                    'status' => true,
-                    'user'=>$user,
-                    'message' => 'User Created Successfully',
-                    'token' => $user->createToken("API TOKEN")->plainTextToken
-                ], 200);
-            }catch (\Throwable $th) {
-                return response()->json([
-                    'status' => false,
-                    'message' => $th->getMessage()
-                ], 500);
-            }
+            ]));
+            // return $data['user'];
+            return Response::Success($data['user'], $data['message']);
+            }catch(\Throwable $th){
+            $message= $th->getMessage();
+            return Response::Error($data,$message);
         }
+    }
      
 
     public function logout()
     {
-        Auth::user()->currentAccessToken()->delete() ;
-        return response()->json([
-            'message'=>'you are logged out'
-        ]);
+        $data= [];
+        try{
+            $data = $this->userService->logout();
+            return Response::Success($data['user'], $data['message'],$data['code']);
+        }catch(\Throwable $th){
+            $message= $th->getMessage();
+            return Response::Error($data,$message);
+        }
     }
 
     
